d55b5f0567f7fd451b7ab33d77d4f08ac7cc4513
Alex Titov <54718346+alextitov19@users.noreply.github.com>
2023-09-05 18:31:43 -0700

    animation working for work experience card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import PacManDivider from "./PacManDivider";
 import './App.css'
+import WorkExperience from "./WorkExperience";
 
 function App() {
   return <div>
     <Header />
     <PacManDivider />
+    <WorkExperience />
   </div >
 }
 
---
+++ src/WorkExperience.tsx
@@ -0,0 +1,7 @@
+import './WorkExperience.css'
+
+function WorkExperience() {
+    return <div className="box"></div>
+}
+
+export default WorkExperience
