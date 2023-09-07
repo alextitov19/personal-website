56d351e394dadedafe857e86123b7aebd4072649
Alex Titov <54718346+alextitov19@users.noreply.github.com>
2023-09-07 15:54:52 -0700

    Update Header.tsx

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ import './Header.css'
 function Header() {
     return <div className="header-main">
         <button className='button-85'>
-            <img src='../public/profile.jpg' className="header-image" alt="..."></img>
+            <img src='/profile.jpg' className="header-image" alt="..."></img>
         </button>
         <div className='header-text'>
             <h1 className='animate-charcter'>Alex Titov</h1>
