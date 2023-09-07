2a44c0ac590f945623fc0d3f8e20cba3a609b712
Alex Titov <54718346+alextitov19@users.noreply.github.com>
2023-09-07 14:11:32 -0700

    card

--- src/Skills.tsx
+++ src/Skills.tsx
@@ -1,7 +1,22 @@
 import './Skills.css'
 
 function Skills() {
-    return <div>Skills</div>
+    return <div>
+        <article className="mycustomcard">
+            <h3>Border Animation</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nostrum quam
+                mollitia sit aliquam omnis nisi neque nulla, ipsa obcaecati eveniet
+                incidunt doloribus quaerat at totam nemo exercitationem, officiis
+                quisquam! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Necessitatibus, quisquam perferendis
+            </p>
+            <span className="top"></span>
+            <span className="right"></span>
+            <span className="bottom"></span>
+            <span className="left"></span>
+        </article>
+    </div>
 }
 
 export default Skills
