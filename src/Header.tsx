11d77daabe13728783b505bc61eebdb8e1526750
Alex Titov <54718346+alextitov19@users.noreply.github.com>
2023-09-13 16:58:37 -0700

    p2

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,28 +1,38 @@
-import './Header.css'
+import "./Header.css";
 
 function Header() {
-    return <div className="header-main">
-        <button className='button-85'>
-            <img src='/profile.jpg' className="header-image" alt="..."></img>
-        </button>
-        <div className='header-text'>
-            <h1 className='animate-charcter'>Alex Titov</h1>
-            <h3>Full-Stack Developer</h3>
-            <h6>Comp Sci & Eng @ UC Merced</h6>
-            <div className='links'>
-                <a className="link-text" href="https://mail.google.com/mail/?view=cm&fs=1&to=alextitov203@gmail.com">
-                    <i className="bi bi-envelope"></i>
-                </a>
-                <a className="link-middle-margin" href="https://github.com/alextitov19">
-                    <i className="bi bi-github"></i>
-                </a>
-                <a className="link-text" href="https://www.linkedin.com/in/alex-titov-b072451a9/">
-                    <i className="bi bi-linkedin"></i>
-                </a>
-
-            </div>
+  return (
+    <div className="header-main">
+      <button className="button-85">
+        <img src="/profile2.jpg" className="header-image" alt="..."></img>
+      </button>
+      <div className="header-text">
+        <h1 className="animate-charcter">Alex Titov</h1>
+        <h3>Full-Stack Developer</h3>
+        <h6>Comp Sci & Eng @ UC Merced</h6>
+        <div className="links">
+          <a
+            className="link-text"
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=alextitov203@gmail.com"
+          >
+            <i className="bi bi-envelope"></i>
+          </a>
+          <a
+            className="link-middle-margin"
+            href="https://github.com/alextitov19"
+          >
+            <i className="bi bi-github"></i>
+          </a>
+          <a
+            className="link-text"
+            href="https://www.linkedin.com/in/alex-titov-b072451a9/"
+          >
+            <i className="bi bi-linkedin"></i>
+          </a>
         </div>
-    </div >
+      </div>
+    </div>
+  );
 }
 
 export default Header;
