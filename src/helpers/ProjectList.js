c58f3af800c9d14d6d250e0cbb118e453ace2265
Alex Titov <54718346+alextitov19@users.noreply.github.com>
2024-04-07 18:28:16 -0700

    project list

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,14 @@
+import bfn from "../assets/bfn.png";
+import parkpals from "../assets/parkpals.png";
+import unity from "../assets/unity.png";
+import gopnik from "../assets/gopnik.png";
+import golf from "../assets/golf.png";
+
+export const ProjectList = [
+  { name: "ParkPals", image: parkpals },
+  { name: "Best Friends Network", image: bfn },
+  { name: "Cuberun", image: unity },
+  { name: "ShotClock", image: unity },
+  { name: "Gopnik's Adventure", image: gopnik },
+  { name: "Golf Simulator", image: golf },
+];
