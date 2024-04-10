2158d401bcd1f1f516d8e5a3eb95efa04aef98b5
Alex Titov <54718346+alextitov19@users.noreply.github.com>
2024-04-10 12:27:53 -0700

    tos

--- src/pages/TermsOfService.js
+++ src/pages/TermsOfService.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import "../styles/TermsOfService.css";
 function TermsOfService() {
-  return <div>TermsOfService</div>;
+  return (
+    <div className="container">
+      <h1>Terms of Service</h1>
+      <p>Last updated: April 08, 2024</p>
+      <p>
+        This Privacy Policy describes Our policies and procedures on the
+        collection, use and disclosure of Your information when You use the
+        Service and tells You about Your privacy rights and how the law protects
+        You.
+      </p>
+      <p>
+        We use Your Personal data to provide and improve the Service. By using
+        the Service, You agree to the collection and use of information in
+        accordance with this Privacy Policy.
+      </p>
+      <h2>Interpretation and Definitions</h2>
+      <h3>Interpretation</h3>;
+    </div>
+  );
 }
 
 export default TermsOfService;
