635b0b8ce42de679e0581a9df015d9f4d49d7a9e
Alex Titov <54718346+alextitov19@users.noreply.github.com>
2025-09-27 14:27:35 -0700

    Update Navbar styles and Docker build script
    Improved Navbar CSS for better layout and responsiveness, including changes to positioning, flex direction, and link/button display. Updated upload_new_image.sh to use Docker build with --no-cache and incremented image version to v1.1.0.

--- upload_new_image.sh
+++ upload_new_image.sh
@@ -1,7 +1,7 @@
-docker build --platform linux/amd64 -t personal-website:v1.0.6 .
+docker build --platform linux/amd64  --no-cache -t personal-website:v1.1.0 .
 
 aws ecr get-login-password --profile personal --region us-east-1 | docker login --username AWS --password-stdin 471112521203.dkr.ecr.us-east-1.amazonaws.com
 
-docker tag personal-website:v1.0.6 471112521203.dkr.ecr.us-east-1.amazonaws.com/personal-website:v1.0.6
+docker tag personal-website:v1.1.0 471112521203.dkr.ecr.us-east-1.amazonaws.com/personal-website:v1.1.0
 
-docker push 471112521203.dkr.ecr.us-east-1.amazonaws.com/personal-website:v1.0.6
+docker push 471112521203.dkr.ecr.us-east-1.amazonaws.com/personal-website:v1.1.0
